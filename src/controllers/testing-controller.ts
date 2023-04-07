950e0c06bc590824d7f756507821698604a1d7fc
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2023-04-07 14:37:35 +0300

    test 4.1

--- src/controllers/testing-controller.ts
+++ src/controllers/testing-controller.ts
@@ -3,11 +3,16 @@ import {postRepository} from "../repositories/post-repository";
 import {userRepository} from "../repositories/user-repositpry";
 import {commentRepository} from "../repositories/comment-repository";
 import {securityDevicesRepository} from "../repositories/securityDevices-repository";
+import {securityDevicesService} from "../services/securityDevices-service";
+import {HTTP_STATUSES} from "../http_statuses";
+import {Request, Response} from "express";
 
 
 export const testingController = {
 
     async clearDB() {
+        const allSession = await securityDevicesRepository.getAllUserSessions({})
+        console.log('не заебумба', allSession)
         await blogRepository.deleteAllBlogs()
         await postRepository.deleteAllPosts()
         await userRepository.deleteAllUser()
